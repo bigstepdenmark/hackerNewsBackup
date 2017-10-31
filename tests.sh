9b94b284e0d3a8bd64194f7ecaeb089b5e9e3e4e
Ismail Cam <i@bigstep.dk>
2017-10-31 20:59:59 +0100

    Refactoring

--- tests.sh
+++ tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Trigger deployment
-# Replace the url below with your Forge/Laravel url
 curl -s 'https://forge.laravel.com/servers/154829/sites/403046/deploy/http?token=oPog0OWOVJDScY6DTT5hZgRziXgAtuQOh6CUPseS';
 echo 'Deployment triggered!'
