e57942aa4647bee067f28a0e7bf99cc96c8367d1
Ismail Cam <i@bigstep.dk>
2017-11-02 10:00:57 +0100

    Add the new deployment trigger

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Trigger deployment
-curl -s 'https://envoyer.io/deploy/lRWSGCknGgkGKCtrNTBQEDJVDlUgr9SafjiITUGy';
+curl -s 'https://envoyer.io/deploy/lRWSGCknGgkGKCtrNTBQEDJVDlUgr9SafjiITUGy'
 echo 'Deployment triggered!'
